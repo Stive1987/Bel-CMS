50aa1da992d70c01f29d6d386da846b4c05d5a90
Determe Stive <stivedeterme@msn.com>
2019-01-13 15:04:15 +0100

    + add page blog

---
+++ pages/blog/controller.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Bel-CMS [Content management system]
+ * @version 0.0.1
+ * @link http://www.bel-cms.be
+ * @link http://www.stive.eu
+ * @license http://opensource.org/licenses/GPL-3.0 copyleft
+ * @copyright 2014-2016 Bel-CMS
+ * @author Stive - mail@stive.eu
+ */
+
+if (!defined('CHECK_INDEX')) {
+	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Direct access forbidden');
+	exit(ERROR_INDEX);
+}
+
+class Blog extends Pages
+{
+	var $models = array('ModelsBlog');
+
+	function index ()
+	{
+		//$set['pagination'] = $this->pagination($_SESSION['pages']->blog->config['MAX_BLOG'], 'blog', TABLE_PAGES_BLOG);
+		$set['blog'] = $this->ModelsBlog->GetBlog();
+		$this->set($set);
+		$this->render('index');
+	}
+
+	function readmore ($name = false, $id = false)
+	{
+		$set = array();
+		$set['blog'] = $this->ModelsBlog->GetBlog($id);
+		if (count($set['blog']) == 0) {
+			$this->error(BLOG, NAME_OF_THE_UNKNOW, 'danger');
+			return;
+		} else {
+			$this->ModelsBlog->NewView($id);
+		}
+		$this->set($set);
+		$this->render('readmore');
+	}
+}
